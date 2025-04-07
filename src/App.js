8612a23988bc21a685278a68375cfb93f3cb4db9
paritosh kumar <paritosh.k@lightmetrics.co>
2025-04-07 15:30:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState, useRef} from "react";
 function App() {
+
+
+  const [ws,setWs] = useState();
+  const [text, setText] = useState("");
+	const [localTrack, setLocalTrack] = useState(null);
+const localVideo = useRef(null);
+	const peerConnection = useRef(null);
+  useEffect(()=>{
+
+
+	  try{
+	navigator.mediaDevices.getDisplayMedia({
+	audio: true,
+		video: true
+	}).then(stream=>{
+	localVideo.current.srcObject = stream;
+		
+	});
+		  const peerConnection = new RTCPeerConnection({})
+	  }
+	  catch(error){
+	  }
+    const ws = new WebSocket('ws://localhost:3000');
+    setWs(ws)
+
+    ws.addEventListener('open', (event) => {
+      ws.send('Hello from client');
+    })
+
+
+    ws.addEventListener('message', (message) => {
+      console.log(`Received from server: ${message}`);
+    });
+
+
+    ws.addEventListener('close', (message) => {
+      console.log(`Disconnected from the server`);
+    });
+
+  },[])
+
+  function sendMessageToServer(){
+    ws.send(text);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type={"text"} value={text} onChange={(event)=>setText(event.target.value)}/>
+        <button onClick={sendMessageToServer}>Send Event to client</button>
+      <video ref={localVideo} autoPlay muted playsInline style={{width: '300px', height:'400px', border:'1px solid black'}} />
+	  </header>
     </div>
   );
 }
