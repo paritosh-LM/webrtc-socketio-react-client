4fd97723fe3c7445691ad81612d47cafe5daab9e
Paritosh Kumar <paritosh.k@lightmetrics.co>
2025-05-07 13:35:27 +0530

    updated react client

--- src/App.js
+++ src/App.js
@@ -1,58 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
-import {useEffect, useState, useRef} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState, useRef } from "react";
+import { io } from "socket.io-client";
+
 function App() {
-
-
-  const [ws,setWs] = useState();
+  const [ws, setWs] = useState();
   const [text, setText] = useState("");
-	const [localTrack, setLocalTrack] = useState(null);
-const localVideo = useRef(null);
-	const peerConnection = useRef(null);
-  useEffect(()=>{
+  const [offer, setOffer] = useState(null);
+  const [localTrack, setLocalTrack] = useState(null);
+  const [stream, localStream] = useState(null);
+  const localVideo = useRef(null);
+  const peerConnection = useRef(null);
+  const socketIo = useRef(null);
 
-
-	  try{
-	navigator.mediaDevices.getDisplayMedia({
-	audio: true,
-		video: true
-	}).then(stream=>{
-	localVideo.current.srcObject = stream;
-		
-	});
-		  const peerConnection = new RTCPeerConnection({})
-	  }
-	  catch(error){
-	  }
-    const ws = new WebSocket('ws://localhost:3000');
-    setWs(ws)
-
-    ws.addEventListener('open', (event) => {
-      ws.send('Hello from client');
-    })
-
-
-    ws.addEventListener('message', (message) => {
-      console.log(`Received from server: ${message}`);
+  const initializePeerConnection = (iceServers) => {
+    if (peerConnection.current) return;
+    peerConnection.current = new RTCPeerConnection({
+      iceServers,
     });
 
+    peerConnection.current.onicecandidate = (event) => {
+      socketIo.current.emit("ice-candidate", event.candidate);
+    };
+  };
 
-    ws.addEventListener('close', (message) => {
-      console.log(`Disconnected from the server`);
+  const createOffer = () => {
+    if (peerConnection.current) {
+      stream.getTracks().forEach((track) => {
+        peerConnection.current.addTrack(track, stream);
+      });
+      peerConnection.current.createOffer().then((offer) => {
+        peerConnection.current.setLocalDescription(offer);
+        setOffer(offer);
+      });
+    }
+  };
+
+  const sendOffer = () => {
+    if (offer) {
+      socketIo.current.emit("offer", offer);
+    }
+  };
+
+  const getTracks = async () => {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      audio: true,
+      video: true,
     });
+    localVideo.current.srcObject = stream;
+    localStream(stream);
+  };
 
-  },[])
+  const initializeSocketIo = () => {
+    if (socketIo.current) return;
+    try {
+      socketIo.current = io("ws://localhost:3001", {
+        path: "/webrtc",
+      });
+      socketIo.current.on("connect", () => {
+        console.log("connected to socket io");
+      });
+      socketIo.current.on("offer", (offer) => {
+        console.log("offer received", offer);
+        peerConnection.current.setRemoteDescription(
+          new RTCSessionDescription(offer)
+        );
+      });
 
-  function sendMessageToServer(){
+      socketIo.current.on("ice-candidate", (candidate) => {
+        peerConnection.current.addIceCandidate(candidate);
+      });
+    } catch (error) {
+      console.log("socketio connection error", error);
+    }
+  };
+
+  useEffect(() => {
+    initializePeerConnection([
+      {
+        urls: ["stun:stun.cloudflare.com:3478", "stun:stun.cloudflare.com:53"],
+      },
+      {
+        urls: [
+          "turn:turn.cloudflare.com:3478?transport=udp",
+          "turn:turn.cloudflare.com:53?transport=udp",
+          "turn:turn.cloudflare.com:3478?transport=tcp",
+          "turn:turn.cloudflare.com:80?transport=tcp",
+          "turns:turn.cloudflare.com:5349?transport=tcp",
+          "turns:turn.cloudflare.com:443?transport=tcp",
+        ],
+        username:
+          "g0640d6950d2deb1c4f0f0efe14ce1e2bd1a467df7ed4ffd971a1501a90b858a",
+        credential:
+          "8eb7c153f2ca51dde45d06fa91ac9fa133017c0f23317872a928924e4b54db24",
+      },
+    ]);
+
+    initializeSocketIo();
+  }, []);
+
+  function sendMessageToServer() {
     ws.send(text);
   }
+
+  function sendDeviceConnection(){
+       socketIo.current.emit("device-connected", true);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <input type={"text"} value={text} onChange={(event)=>setText(event.target.value)}/>
+        <input
+          type={"text"}
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
         <button onClick={sendMessageToServer}>Send Event to client</button>
-      <video ref={localVideo} autoPlay muted playsInline style={{width: '300px', height:'400px', border:'1px solid black'}} />
-	  </header>
+        <button onClick={getTracks}>Get Tracks</button>
+        <button onClick={createOffer}>Create offer</button>
+        <button onClick={sendOffer}>Send Offer</button>
+        <button onClick={sendDeviceConnection}>Send Device Connected Status</button>
+        <video
+          ref={localVideo}
+          autoPlay
+          muted
+	  controls
+          playsInline
+          style={{ width: "300px", height: "400px", border: "1px solid black" }}
+        />
+      </header>
     </div>
   );
 }
